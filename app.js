456acc93d26d514124c224fb2867e14382f8e9f3
Shouttomi <123111039+Shouttomi@users.noreply.github.com>
2023-03-15 22:41:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+
 const express = require("express");
 const app = express();
 
@@ -37,6 +38,10 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/',(req,res)=>{
+  res.send('jobs api')
+})
 
 // routes
 app.use("/api/v1/auth", authRouter);
