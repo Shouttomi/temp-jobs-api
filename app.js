8910684fd57388873da48fd54c0e511e86a39eee
Shouttomi <123111039+Shouttomi@users.noreply.github.com>
2023-03-16 16:22:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 require("express-async-errors");
 
+
 //extra security packages
 const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 const express = require("express");
 const app = express();
@@ -39,9 +44,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+
 app.get('/',(req,res)=>{
-  res.send('jobs api')
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use("/api/v1/auth", authRouter);
